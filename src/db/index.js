5809fe89c8a1b701a0ca855e774e8c9466ba4e79
R.Kloppers <rhenierkloppers124@gmail.com>
2019-06-17 15:38:01 +0200

    commiting before merge

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const options = {
   useMongoClient: true,
 };
 
-mongoose.connect(`mongodb://${config.mlabUsername}:${config.mlabPassword}@ds125183.mlab.com:25183/sundaystories`, options);
+mongoose.connect(`mongodb://${config.mlabUsername}:${config.mlabPassword}@ds159236-a0.mlab.com:59236,ds159236-a1.mlab.com:59236/sunday-prod?replicaSet=rs-ds159236`, options);
 
 export const db = mongoose.connection;
 
